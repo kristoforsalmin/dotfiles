bcd8ae297b28abe291eb9076a57466cf97dae488
Kristofor Salmin <4795426+kristoforsalmin@users.noreply.github.com>
2017-09-19 18:10:13 +0300

    Automate installation of Visual Studio Code extensions

--- brew.bash
+++ brew.bash
@@ -2,6 +2,17 @@
 
 cd "$(dirname $BASH_SOURCE)"
 
+# Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap homebrew/bundle
+
+# Install dependencies
 brew bundle
+
+# Install Visual Studio Code extensions
+code \
+  --install-extension dbaeumer.vscode-eslint \
+  --install-extension EditorConfig.EditorConfig \
+  --install-extension ms-vscode.sublime-keybindings \
+  --install-extension msjsdiag.debugger-for-chrome \
+  --install-extension shinnn.stylelint
