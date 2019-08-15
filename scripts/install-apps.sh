b59380a1654e3babd0fab923a58456030805bad9
Kristofor Salmin <4795426+kristoforsalmin@users.noreply.github.com>
2019-08-15 22:42:37 +0300

    Add missing Atom extensions

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -10,4 +10,10 @@ brew tap homebrew/bundle
 brew bundle --file=../home/Brewfile
 
 # Install Atom extensions
-apm install editorconfig sort-lines
+apm install \
+  editorconfig \
+  linter \
+  linter-eslint \
+  linter-stylelint \
+  linter-ui-plus \
+  sort-lines
