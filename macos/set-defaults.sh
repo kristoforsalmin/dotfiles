eae3231dc2e59b0f9e010977e06d89c4a6c8fdec
Kristofor Salmin <4795426+kristoforsalmin@users.noreply.github.com>
2022-01-11 23:38:52 +0100

    Add macOS preferences

---
+++ macos/set-defaults.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Use list view in all Finder windows by default
+defaults write com.apple.finder FXPreferredViewStyle -string 'Nlsv'
+
+# Set the icon size of Dock items to 48 pixels
+defaults write com.apple.dock tilesize -int 48
